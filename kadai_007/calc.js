98af04085b0eb75a384c8d5a803a972b81741fc9
yuitHub625 <luckylukia0626@gmail.com>
2025-04-08 23:56:44 +0900

    kadai_007を追加

---
+++ kadai_007/calc.js
@@ -0,0 +1,21 @@
+num = 15;
+
+// 変数(num)が3の倍数の場合
+if ((num % 3) === 0) {
+  console.log('3の倍数です');
+}
+
+// 変数(num)が5の倍数の場合
+else if ((num % 5) === 0) {
+  console.log('5の倍数です');
+}
+
+// 変数(num)が3と5の倍数の場合
+else if ((num % 3) === 0 && (num % 5) === 0) {
+  console.log('3と5の倍数です');
+}
+
+// それ以外の場合
+else {
+  console.log(num);
+}
